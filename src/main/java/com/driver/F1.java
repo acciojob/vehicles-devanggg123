c349fd2f030517ed636d60acdd2619944c286639
devanggg123 <sfdc.devang123@gmail.com>
2022-12-22 13:49:25 +0530

    hoja pls

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,12 +4,12 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name,4,4,"racing",7, isManual,2);
+        super(name,4,2,"racing",7, isManual,2);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        newSpeed =getCurrentSpeed()+ rate;
+         //set the value of new speed by using currentSpeed and rate
+       int newSpeed =getCurrentSpeed()+ rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
